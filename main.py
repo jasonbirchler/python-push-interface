c84b23a91ce8185c5acdc7e5b5aa3afbf6b0698e
Jason Birchler <hugenerd@mac.com>
2025-10-09 14:18:51 -0700

    adds requirements for display interaction simulator works main.py is just a copy of "Interface with the display (dynamic content)" example

--- main.py
+++ main.py
@@ -1,7 +1,79 @@
-#!/usr/bin/env python3
+import push2_python
+import cairo
+import numpy
+import random
+import time
 
-def main():
-    print("Hello, Python Push Interface!")
+# Init Push2
+push = push2_python.Push2(run_simulator=True)
 
-if __name__ == "__main__":
-    main()
+# Init dictionary to store the state of encoders
+encoders_state = dict()
+max_encoder_value = 100
+for encoder_name in push.encoders.available_names:
+    encoders_state[encoder_name] = {
+        'value': int(random.random() * max_encoder_value),
+        'color': [random.random(), random.random(), random.random()],
+    }
+last_selected_encoder = list(encoders_state.keys())[0]
+
+# Function that generates the contents of the frame do be displayed
+def generate_display_frame(encoder_value, encoder_color, encoder_name):
+
+    # Prepare cairo canvas
+    WIDTH, HEIGHT = push2_python.constants.DISPLAY_LINE_PIXELS, push2_python.constants.DISPLAY_N_LINES
+    surface = cairo.ImageSurface(cairo.FORMAT_RGB16_565, WIDTH, HEIGHT)
+    ctx = cairo.Context(surface)
+
+    # Draw rectangle with width proportional to encoders' value
+    ctx.set_source_rgb(*encoder_color)
+    ctx.rectangle(0, 0, WIDTH * (encoder_value/max_encoder_value), HEIGHT)
+    ctx.fill()
+
+    # Add text with encoder name and value
+    ctx.set_source_rgb(1, 1, 1)
+    font_size = HEIGHT//3
+    ctx.set_font_size(font_size)
+    ctx.select_font_face("Arial", cairo.FONT_SLANT_NORMAL, cairo.FONT_WEIGHT_NORMAL)
+    ctx.move_to(10, font_size * 2)
+    ctx.show_text("{0}: {1}".format(encoder_name, encoder_value))
+
+    # Turn canvas into numpy array compatible with push.display.display_frame method
+    buf = surface.get_data()
+    frame = numpy.ndarray(shape=(HEIGHT, WIDTH), dtype=numpy.uint16, buffer=buf)
+    frame = frame.transpose()
+    return frame
+
+# Set up action handlers to react to encoder touches and rotation
+@push2_python.on_encoder_rotated()
+def on_encoder_rotated(push, encoder_name, increment):
+    def update_encoder_value(encoder_idx, increment):
+        updated_value = int(encoders_state[encoder_idx]['value'] + increment)
+        if updated_value < 0:
+            encoders_state[encoder_idx]['value'] = 0
+        elif updated_value > max_encoder_value:
+            encoders_state[encoder_idx]['value'] = max_encoder_value
+        else:
+            encoders_state[encoder_idx]['value'] = updated_value
+
+    update_encoder_value(encoder_name, increment)
+    global last_selected_encoder
+    last_selected_encoder = encoder_name
+
+@push2_python.on_encoder_touched()
+def on_encoder_touched(push, encoder_name):
+    global last_selected_encoder
+    last_selected_encoder = encoder_name
+
+# Draw method that will generate the frame to be shown on the display
+def draw():
+    encoder_value = encoders_state[last_selected_encoder]['value']
+    encoder_color = encoders_state[last_selected_encoder]['color']
+    frame = generate_display_frame(encoder_value, encoder_color, last_selected_encoder)
+    push.display.display_frame(frame, input_format=push2_python.constants.FRAME_FORMAT_RGB565)
+
+# Now start infinite loop so the app keeps running
+print('App runnnig...')
+while True:
+    draw()
+    time.sleep(1.0/30)  # Sart drawing loop, aim at ~30fps
